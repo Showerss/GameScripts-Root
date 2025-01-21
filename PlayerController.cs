bb00ad11f6c8f34e6e9d6626619658d78c5373c1
Phillip <phillip.banky@gmail.com>
2025-01-21 09:31:42 -0800

    doing more inside of the movement controls

--- PlayerController.cs
+++ PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
         Elder
     }
 
-    #region Player Status
+    #region Player Status bools (blocking, dead, alive, running, etc)
     public bool isGrounded = false; //is the player on the ground
     public bool isRunning = false; //is the player running
     public bool isCrouching = false; //is the player crouching
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
 
 
 
-    #region Player Construction
+    #region Player Construction declarations (rigidbody, character type, camera)
     public Rigidbody rb;  //rigidbody component
     public CharacterType characterType; //type of character
     public Camera playerCamera; //camera that follows the player
@@ -45,8 +45,7 @@ public class PlayerController : MonoBehaviour
 
 
 
-
-    #region Player Movement
+    #region Player health/stam/speed values and floats (walking speed, running speed, health, stamina, etc)
     [Header("Player Movement Settings")]
     [SerializeField] private float WalkingSpeed = 10.0f;
     [SerializeField] private float RunningSpeed = 20.0f;
@@ -54,7 +53,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float JumpForce = 10.0f;
     [SerializeField] private float PlayerHeight = 2.0f;
     [SerializeField] private float Gravity = 9.8f;
-    #endregion
+    
 
     [Space(25)]
     
@@ -67,7 +66,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float Stamina = 100.0f;
     [SerializeField] private float StaminaRegen = 1.0f;
     [SerializeField] private float StaminaDepletion = 1.0f;
-
+    #endregion
 
     [Space(25)]
     [Header("Crosshair Settings")]
@@ -94,24 +93,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() //update is called every frame
     {
-        
+        //camera
+        //sprint
+        //crouch
+        //jump
     }
 
     void FixedUpdate() //fixed update is called once per physics update
     {
-
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight / 2 + 0.1f); //this checks if the player is on the ground
     }
 
 
-    #region Player Movement Methods
+    #region Player Movement Methods (Idle, Jump, MoveForward, MoveBackward, MoveLeft, MoveRight, Sprint, Crouch, Interact)
     void Idle()
     {
         //should stop the player from moving
+        //regains stamina
         transform.localScale = new Vector3(1, 1, 1);
     }
     void Jump()
     {
-        rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        if(isGrounded)
+        {
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse); //forcemode impulse is used to apply the force instantly
+            isJumping = true;
+        }
     }
 
     void MoveForward()
@@ -136,13 +143,25 @@ public class PlayerController : MonoBehaviour
     void Sprint()
     {
         rb.AddForce(playerCamera.transform.forward * RunningSpeed);
+        isRunning = true;
         //should zoom the camera in some way as well to show the player is running
+        //depletes stamina
     }
     void Crouch()
     {
-        rb.AddForce(playerCamera.transform.forward * CrouchingSpeed);
-        transform.localScale = new Vector3(1, 0.5f, 1);
-        //should lower the camera to show the player is crouching
+        if(isCrouching)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            rb.AddForce(playerCamera.transform.forward * WalkingSpeed);
+            isCrouching = false;
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 0.5f, 1);
+            rb.AddForce(playerCamera.transform.forward * CrouchingSpeed);
+            isCrouching = true;
+        }
+        
     }
     void Interact()
     {
@@ -151,55 +170,63 @@ public class PlayerController : MonoBehaviour
     #endregion
 
 
-    #region Handle Movement
+    #region Handle Movement if statements (HandleMovement)
     void HandleMovement()
     {
+
+        bool isMoving = false;
+
         if(Input.GetKey(controls.MoveForward))
         {
             MoveForward();
+            isMoving = true;
         }
         if(Input.GetKey(controls.MoveBackward))
         {
             MoveBackward();
+            isMoving = true;
         }
         if(Input.GetKey(controls.MoveLeft))
         {
             MoveLeft();
+            isMoving = true;
         }
         if(Input.GetKey(controls.MoveRight))
         {
             MoveRight();
+            isMoving = true;
         }
         if(Input.GetKeyDown(controls.Jump))
         {
             Jump();
+            isJumping = true;
         }
         if(Input.GetKey(controls.Run))
         {
             Sprint();
-        }
-        else
-        {
-            Idle();
+            isRunning = true;
+            isMoving = true;
         }
 
         if(Input.GetKey(controls.Crouch))
         {
             Crouch();
         }
-        else
-        {
-            Idle();
-        }
         if(Input.GetKeyDown(controls.Interact))
         {
             //Interact();
         }
+
+        if(!isMoving)
+        {
+            Idle();
+        }
+
     }
     #endregion
 
 
-    #region Collapsable Menus
+    #region Collapsable Menus Serializables (PlayerControls)
     [System.Serializable]
     public class PlayerControls
     {
